ae398113c53f94a7883c6206c9937aeb860c8693
thcoffee <48222283@qq.com>
2017-04-11 13:47:00 +0800

    2017-04-11 backup

---
+++ _modules/appserver.py
@@ -0,0 +1,33 @@
+import os,time
+import salt.client
+def getpid(msg):
+    time.sleep(10)
+    return(msg+str(os.getpid()))
+
+def zu(**msg):
+    return(msg['name'])
+    
+def get_file():
+    caller = salt.client.Caller()
+    a=caller.cmd('cp.get_file','salt://Python-3.4.6.tgz','/root/Python-3.4.6.tgz')
+    return a
+
+def getdate():
+    time.sleep(10)
+    return _getDate()
+
+def _getDate():
+    return(str(time.time()))    
+
+def wait():
+    a=str(time.time())
+    time.sleep(int(a[-1]))
+    return
+
+def mark(taskid):
+    with open('/root/test.log','a') as myfile:
+        myfile.write(taskid+' begin\n')
+        myfile.flush()
+        time.sleep(10)
+        myfile.write(taskid+' end\n')
+        myfile.flush()
--- bin/backup.sh
+++ bin/backup.sh
@@ -2,6 +2,7 @@ cd /home/deployuser/aom
 echo `date`
 git add bin
 git add conf
+git add _modules
 git add .gitignore
 git add README.md
 git commit -m "`date +%Y-%m-%d` backup"
