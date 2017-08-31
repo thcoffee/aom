482a995f847b638715c63255d61f18a872093e64
thcoffee <48222283@qq.com>
2017-09-01 00:00:02 +0800

    2017-09-01 backup

--- web/aom/admin.py
+++ web/aom/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import AomCustom
+from .models import AomProject
+from .models import AomApp
+from .models import AomAppSt
+from .models import AomEnvironment
+from .models import AomNode
+
+
+class AomAppStAdmin(admin.StackedInline):
+    model = AomAppSt
+    #max_num = 1
+    
+class AomAppAdmin(admin.ModelAdmin):
+    inlines = [AomAppStAdmin]
+
+admin.site.register(AomCustom)
+admin.site.register(AomProject)
+admin.site.register(AomApp,AomAppAdmin)
+admin.site.register(AomEnvironment)
+admin.site.register(AomNode)
--- web/aom/models.py
+++ web/aom/models.py
@@ -1,3 +1,142 @@
 from django.db import models
 
 # Create your models here.
+class AomApp(models.Model):
+    appid = models.AutoField(primary_key=True)
+    appname = models.CharField(max_length=50, blank=True, null=True)
+    appcname = models.CharField(max_length=50, blank=True, null=True)
+    projectid = models.ForeignKey('AomProject', models.DO_NOTHING, db_column='projectid', blank=True, null=True)
+    percentdep = models.IntegerField(db_column='percentDep', blank=True, null=True)  # Field name made lowercase.
+    codectrl = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'aom_app'
+    def __str__(self):
+        return(str(self.projectid)+','+self.appname)
+
+class AomApp2Jvm(models.Model):
+    app2jvmid = models.AutoField(primary_key=True)
+    appid = models.ForeignKey(AomApp, models.DO_NOTHING, db_column='appid', blank=True, null=True)
+    appserverid = models.ForeignKey('AomAppserver', models.DO_NOTHING, db_column='appserverid', blank=True, null=True)
+    status = models.CharField(max_length=1024, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'aom_app2jvm'
+
+
+class AomAppSt(models.Model):
+    appid = models.ForeignKey(AomApp, models.DO_NOTHING, db_column='appid', primary_key=True)
+    stproject = models.CharField(db_column='stProject', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    stview = models.CharField(db_column='stView', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    stfolder = models.CharField(db_column='stFolder', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    mvnprofile = models.CharField(db_column='mvnProfile', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    appwar = models.CharField(db_column='appWar', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    approot = models.CharField(db_column='appRoot', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    checkpage = models.CharField(db_column='checkPage', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    checkkey = models.CharField(db_column='checkKey', max_length=50, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'aom_app_st'
+
+
+class AomAppserver(models.Model):
+    appserverid = models.AutoField(primary_key=True)
+    appserver_type_id = models.CharField(max_length=50, blank=True, null=True)
+    envid = models.ForeignKey('AomEnvironment', models.DO_NOTHING, db_column='envid', blank=True, null=True)
+    nodeid = models.ForeignKey('AomNode', models.DO_NOTHING, db_column='nodeid', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'aom_appserver'
+
+
+class AomAppserverTomcat(models.Model):
+    appserverid = models.ForeignKey(AomAppserver, models.DO_NOTHING, db_column='appserverid', primary_key=True)
+    http_port = models.IntegerField(blank=True, null=True)
+    basedir = models.CharField(db_column='baseDir', max_length=1024, blank=True, null=True)  # Field name made lowercase.
+    docbase = models.CharField(db_column='docBase', max_length=1024, blank=True, null=True)  # Field name made lowercase.
+    appbase = models.CharField(db_column='appBase', max_length=1024, blank=True, null=True)  # Field name made lowercase.
+    workdir = models.CharField(db_column='workDir', max_length=1024, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'aom_appserver_tomcat'
+
+
+class AomAppserverType(models.Model):
+    appserver_type_id = models.AutoField(primary_key=True)
+    appserver_type_name = models.CharField(max_length=50, blank=True, null=True)
+    appserver_type_version = models.CharField(max_length=50, blank=True, null=True)
+    appserver_type_bit = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'aom_appserver_type'
+ 
+
+class AomCustom(models.Model):
+    customid = models.AutoField(primary_key=True)
+    customname = models.CharField(max_length=50, blank=True, null=True)
+    customcname = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'aom_custom'
+    def __str__(self):
+        return(self.customname)
+
+class AomEnvironment(models.Model):
+    envid = models.AutoField(primary_key=True)
+    envname = models.CharField(max_length=50, blank=True, null=True)
+    envcname = models.CharField(max_length=50, blank=True, null=True)
+    projectid = models.ForeignKey('AomProject', models.DO_NOTHING, db_column='projectid', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'aom_environment'
+    
+    def __str__(self):
+        return(str(self.projectid)+','+self.envname)
+
+
+class AomNode(models.Model):
+    nodeid = models.CharField(primary_key=True, max_length=50)
+    osid = models.ForeignKey('AomOs', models.DO_NOTHING, db_column='osid', blank=True, null=True)
+    customid = models.ForeignKey(AomCustom, models.DO_NOTHING, db_column='customid', blank=True, null=True)
+    ip = models.CharField(max_length=15, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'aom_node'
+
+    def __str__(self):
+        return(self.nodeid+' '+self.ip)
+
+class AomOs(models.Model):
+    osid = models.IntegerField(primary_key=True)
+    osname = models.CharField(max_length=50, blank=True, null=True)
+    osversion = models.CharField(max_length=50, blank=True, null=True)
+    osbit = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'aom_os'
+    
+    def __str__(self):
+        return(self.osname+self.osversion+self.osbit)
+
+class AomProject(models.Model):
+    projectid = models.AutoField(primary_key=True)
+    projectname = models.CharField(max_length=50, blank=True, null=True)
+    projectcname = models.CharField(max_length=50, blank=True, null=True)
+    customid = models.ForeignKey(AomCustom, models.DO_NOTHING, db_column='customid', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'aom_project'
+    
+    def __str__(self):
+        return(str(self.customid)+','+str(self.projectname))
