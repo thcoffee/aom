fcd8fafa6f37934bf3130d05f70a634b5fe3b835
thcoffee <48222283@qq.com>
2017-10-18 00:00:02 +0800

    2017-10-18 backup

--- bin/functions/task.py
+++ bin/functions/task.py
@@ -10,8 +10,9 @@ from functions import installSoftWare
 from functions import db as dbO
 stdLogger = logging.getLogger('root')
 
+#任务线程
 class taskThreadObj(threading.Thread):            
-    
+    #构造方法
     def __init__(self,**kwages):
         threading.Thread.__init__(self)
         self.systemDict=kwages['systemDict']
@@ -40,48 +41,55 @@ class taskThreadObj(threading.Thread):
             try:
                 self.processTask()
             except Exception as info:
+                self.systemDict['thread']['task'][self.name]['taskId']=None
                 stdLogger.error(traceback.format_exc())
             time.sleep(5)
-        
-    def processTask(self):
-        #db=pymysql.connect(**self.dbcon)
-        db=dbO.opMysqlObj(**self.dbcon) 
+            
+    #获取任务
+    def _getTask(self,**kwages):
+        db=kwages['db']
         data=db.getData(sql="select * from aom_task_before where taskstatus=1 for update")   
         for i in data:
             if 1 == 1:
                 db.putData(sql='update aom_task_before set taskstatus=2 where taskid=%s'%(i['taskid']))
                 db.commit()
-                stdLogger.debug("".join([self.name,str(i)]))                    
-                self.systemDict['thread']['task'][self.name]['taskId']=i['taskid']
-                i['db']=db
-                taskDict=eval(i['taskcontent'])
-                stdLogger.debug(taskDict)
-                if i['tasktype']==u'test1':
-                    t=test1.test1(**i)
-                    t.run() 
-                elif i['tasktype']==u'installsoftware':
-                    if taskDict['name']=='jdk':
-                        t=installSoftWare.jdk(**i)
-                        t.install()
-                    elif taskDict['name']=='nginx':
-                        t=installSoftWare.nginx(**i)
-                        t.install()
-                    elif taskDict['name']=='tomcat':
-                        t=installSoftWare.tomcat(**i)
-                        t.install()
-                    else:
-                        db.putData(sql="insert into aom_msg (msgdate,msgtype,msgcontent,taskid)values (now(),1,'%s',%s)" %("不支持该软件安装.",i['taskid']))
+                return({'status':True,'obj':i})
+        db.commit()        
+        return({'status':False})
+        
+    #处理任务
+    def processTask(self):
+        db=dbO.opMysqlObj(**self.dbcon) 
+        taskInfo=self._getTask(**{'db':db})        
+        if taskInfo['status']:
+            i=taskInfo['obj']
+            i['db']=db
+            stdLogger.info(" ".join([self.name,str(i['taskid']),' begin.']))                    
+            self.systemDict['thread']['task'][self.name]['taskId']=i['taskid']
+            taskDict=eval(i['taskcontent'])
+            if i['tasktype']==u'test1':
+                t=test1.test1(**i)
+                t.run() 
+            elif i['tasktype']==u'installsoftware':
+                if taskDict['name']=='jdk':
+                    t=installSoftWare.jdk(**i)
+                    t.install()
+                elif taskDict['name']=='nginx':
+                    t=installSoftWare.nginx(**i)
+                    t.install()
+                elif taskDict['name']=='tomcat':
+                    t=installSoftWare.tomcat(**i)
+                    t.install()
                 else:
-                    db.putData(sql="insert into aom_msg (msgdate,msgtype,msgcontent,taskid)values (now(),1,'%s',%s)" %("未知的任务类型.",i['taskid']))
-                stdLogger.debug("".join([self.name,' ',str(i['taskid']),' Processed.']))
-                self.systemDict['thread']['task'][self.name]['taskId']=None
-                db.putData(sql='update aom_task_before set taskstatus=3 where taskid=%s'%(i['taskid']))
-                db.commit()
-                db.putData(sql='insert into aom_task_after select * from aom_task_before where taskid=%s'%(i['taskid']))
-                db.putData(sql='delete from aom_task_before where taskid=%s'%(i['taskid']))
-                db.commit()
-                break
-
+                    db.putData(sql="insert into aom_msg (msgdate,msgtype,msgcontent,taskid)values (now(),1,'%s',%s)" %("不支持该软件安装.",i['taskid']))
+            else:
+                db.putData(sql="insert into aom_msg (msgdate,msgtype,msgcontent,taskid)values (now(),1,'%s',%s)" %("未知的任务类型.",i['taskid']))  
+            db.putData(sql='update aom_task_before set taskstatus=3 where taskid=%s'%(i['taskid']))
+            db.putData(sql='insert into aom_task_after select * from aom_task_before where taskid=%s'%(i['taskid']))
+            db.putData(sql='delete from aom_task_before where taskid=%s'%(i['taskid']))
+            db.commit()
+            self.systemDict['thread']['task'][self.name]['taskId']=None
+            stdLogger.info("".join([self.name,' ',str(i['taskid']),' Processed.']))     
         db.commit()                
         db.close()
     
